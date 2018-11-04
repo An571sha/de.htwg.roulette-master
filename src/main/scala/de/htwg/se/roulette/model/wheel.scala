e29640d638074abc14636955d7cdff3a86b8d038
An571sha <anim_esh@yahoo.co.in>
2018-11-04 15:51:25 +0100

    ModelWorkasheet made blank to resolve Conflit

---
+++ src/main/scala/de/htwg/se/roulette/model/wheel.scala
@@ -0,0 +1,44 @@
+package de.htwg.se.roulette.model
+
+/*This class is resposible for holding the players bet*/
+case class wheel() {
+  var colour: String = "blue"
+  var NumSpins = 0
+  val value = new scala.util.Random(36)
+
+  /*def wheelColor(i: Int): Unit = if ((i eq 1) || (i eq 3) || (i eq 5) || (i eq 7) || (i eq 9) || (i eq 12) || (i eq 14) || (i eq 16) || (i eq 18) || (i eq 19) || (i eq 21) || (i eq 23) || (i eq 25) || (i eq 27) || (i eq 30) || (i eq 32) || (i eq 34) || (i eq 36)) colour = "red"
+  else if (i eq 0) colour = "green"
+  else colour = "black"
+  */
+
+
+  def wheelColor(i: Int): String = {
+    if (i == 1 || i == 3 || i == 5
+      || i == 7 || i == 9 || i == 12
+      || i == 14 || i == 16 || i == 18
+      || i == 19 || i == 21 || i == 23
+      || i == 25 || i == 27 || i == 30
+      || i == 32 || i == 34 || i == 36) {
+      colour = "red"
+
+    } else if(i == 0) {
+        colour = "green"
+      }
+      else {
+        colour = "black"
+      }
+      colour.toString
+    }
+
+
+
+  def spin(): Unit = {
+    NumSpins += NumSpins
+    print("The wheel is Spinning..." + wheelColor(value.nextInt()) + value)
+  }
+
+
+
+
+
+}
