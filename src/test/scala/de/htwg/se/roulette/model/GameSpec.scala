0946ac9bd031f5ecf8db198345c51b2d5db9e29b
Jan Berchtold <Jan.Berchtold@htwg-konstanz.de>
2018-11-10 16:39:26 +0100

    commit2

---
+++ src/test/scala/de/htwg/se/roulette/model/GameSpec.scala
@@ -0,0 +1,13 @@
+package de.htwg.se.roulette.model
+
+import org.scalatest._
+
+class GameSpec extends WordSpec with Matchers {
+  "A number" when {
+    "is even"in {
+      Boolean b = new G
+      x.isEven should be(true)
+    }
+  }
+
+}
